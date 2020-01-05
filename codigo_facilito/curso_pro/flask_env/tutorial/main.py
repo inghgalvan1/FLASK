400f5f5517be9fe9c412923a4046e51ac44bf4f7
Hector Galvan <inghgalva@outlook.com>
2020-01-05 00:15:02 -0600

    Clase 8

--- codigo_facilito/curso_pro/flask_env/tutorial/main.py
+++ codigo_facilito/curso_pro/flask_env/tutorial/main.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return  render_template('index.html')
+    name = "Hector Galvan"
+    course = "Python web"
+    
+    return  render_template('index.html',username=name, coursename=course)
 
 if __name__ == '__main__':
     app.run(debug=True,port = 9000) 
